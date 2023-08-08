dbbe55f68b4428db067fbd13966c283fe80e171b
PlumeSC <supawit.chukiat@gmail.com>
2023-08-08 09:39:36 +0700

    OK

---
+++ 01-If-Else/09-sortNumber/main.js
@@ -0,0 +1,30 @@
+let a = +prompt(`Number A`)
+let b = +prompt(`Number B`)
+let c = +prompt(`Number C`)
+
+
+
+if(!isNaN(a)&&!isNaN(b)&&!isNaN(c)){
+    if(a>b&&a>c){
+        if(b>c){
+            console.log(a,b,c);
+        }else if(c>b){
+            console.log(a,c,b);
+        }
+    }else if(b>a&&b>c){
+        if(a>c){
+            console.log(b,a,c);
+        }else if(a>c){
+            console.log(b,c,a);
+        }
+    }else if(c>a&&c>b){
+        if(a>b){
+            console.log(c,a,b);
+        }else if(a>b){
+            console.log(c,b,a);
+        }
+    }
+}else{
+    console.log(`Not a Number`);
+}
+
